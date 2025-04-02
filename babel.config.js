d70c01f6a482ab314a210b5185413100fd88952d
Prakash Kumar Mandal <prakashkumar.mandal@publicisapient.com>
2025-04-02 12:41:33 +0530

    absolute path setup doner

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'], // Specify the root directory for absolute imports
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@navigation': './src/app/navigation',
+          '@theme': './src/app/theme',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@config': './src/config',
+          '@contexts': './src/contexts',
+          '@hooks': './src/hooks',
+          '@models': './src/models',
+          '@modules': './src/modules',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@utils': './src/utils',
+          '@types': './src/types',
+          // Add more aliases as needed
+        },
+      },
+    ],
+  ],
 };
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import {Text} from 'react-native';
 
 export default function App() {
-  return <Text>sdfsdfsdf</Text>;
+  return <Text>Helllo</Text>;
 }

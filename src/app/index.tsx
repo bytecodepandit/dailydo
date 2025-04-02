840358fd52c6e1c3f905046409c32be136be55d3
Prakash Kumar Mandal <prakashkumar.mandal@publicisapient.com>
2025-04-02 11:48:49 +0530

    Project setup done

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={{flex: 1}}>
-      <Text>sdfsdfsdf</Text>
-    </View>
-  );
+  return <Text>sdfsdfsdf</Text>;
 }
